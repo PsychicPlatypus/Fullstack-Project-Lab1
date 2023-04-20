c14914df47b7d7794fb66409df90d0d569236f1d
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2023-04-20 22:05:43 +0200

    Refactored

--- database.js
+++ database.js
@@ -8,79 +8,79 @@ mongoose.set("strictQuery", false); // Prepare for Mongoose 7
 const mongoDB = process.env.ATLAS_URI;
 
 export async function getAllAlbums() {
-	await mongoose.connect(mongoDB);
-	const albums = await Albums.find({});
-	mongoose.connection.close();
-	return albums;
+    await mongoose.connect(mongoDB);
+    const albums = await Albums.find({});
+    mongoose.connection.close();
+    return albums;
 }
 
 export async function getAlbumByTitle(title) {
-	await mongoose.connect(mongoDB);
-	const album = await Albums.find({ name: title });
-	mongoose.connection.close();
-	return album;
+    await mongoose.connect(mongoDB);
+    const album = await Albums.find({ name: title });
+    mongoose.connection.close();
+    return album;
 }
 
 export async function getAlbumById(id) {
-	await mongoose.connect(mongoDB);
-	const album = await Albums.findOne({ id: id });
-	mongoose.connection.close();
-	return album;
+    await mongoose.connect(mongoDB);
+    const album = await Albums.findOne({ id: id });
+    mongoose.connection.close();
+    return album;
 }
 
 export async function createNewAlbum(album) {
-	await mongoose.connect(mongoDB);
-	if (!album.id) {
-		const newId = await Albums.countDocuments({});
-		album.id = newId;
-	}
-	const count_ = await Albums.countDocuments({
-		$or: [{ id: album.id }],
-	}).exec();
-	if (count_ === 0) {
-		console.log(album.date);
-		const album_ = await Albums.create({
-			id: album.id,
-			name: album.name,
-			year: Date.parse(album.date),
-			artist: album.artist,
-		});
-		mongoose.connection.close();
-		return album_;
-	}
-	mongoose.connection.close();
-	return { message: "Collection already exists." };
+    await mongoose.connect(mongoDB);
+    if (!album.id) {
+        const newId = await Albums.countDocuments({});
+        album.id = newId;
+    }
+    const count_ = await Albums.countDocuments({
+        $or: [{ id: album.id }],
+    }).exec();
+    if (count_ === 0) {
+        console.log(album.date);
+        const album_ = await Albums.create({
+            id: album.id,
+            name: album.name,
+            year: Date.parse(album.date),
+            artist: album.artist,
+        });
+        mongoose.connection.close();
+        return album_;
+    }
+    mongoose.connection.close();
+    return { message: "Collection already exists." };
 }
 
 export async function updateAlbumWithId(album) {
-	await mongoose.connect(mongoDB);
-	const count_ = await Albums.countDocuments({ id: album.id }).exec();
-	if (count_ > 0) {
-		const album_ = Albums.findOneAndUpdate(
-			{ id: album.id },
-			{
-				id: album.id,
-				name: album.name,
-				year: Date.parse(album.date),
-				artist: album.artist,
-			},
-			{ new: true }
-		);
-		mongoose.connection.close();
-		return album_;
-	}
-	mongoose.connection.close();
-	return { message: "Collection not found." };
+    await mongoose.connect(mongoDB);
+    const count_ = await Albums.countDocuments({ id: album.id }).exec();
+    if (count_ > 0) {
+        const album_ = Albums.findOneAndUpdate(
+            { id: album.id },
+            {
+                id: album.id,
+                name: album.name,
+                year: Date.parse(album.date),
+                artist: album.artist,
+            },
+            { new: true }
+        );
+        mongoose.connection.close();
+        return album_;
+    }
+    mongoose.connection.close();
+    return { message: "Collection not found." };
 }
 
 export async function deleteOneAlbum(album) {
-	await mongoose.connect(mongoDB);
-	const count_ = await Albums.countDocuments({ id: album.id }).exec();
-	if (count_ > 0) {
-		const album_ = await Albums.findOneAndDelete({ id: album.id });
-		mongoose.connection.close();
-		return album_;
-	}
-	mongoose.connection.close();
-	return { message: "Collection not found." };
+    await mongoose.connect(mongoDB);
+    const count_ = await Albums.countDocuments({ id: album.id }).exec();
+    if (count_ > 0) {
+        const album_ = await Albums.findOneAndDelete({ id: album.id });
+        mongoose.connection.close();
+        return album_;
+    }
+    mongoose.connection.close();
+    return { message: "Collection not found." };
 }
--- public/main.js
+++ public/main.js
@@ -1,36 +1,36 @@
 let albums;
 
 async function start() {
-	fetch("http://localhost:8000/api/albums")
-		.then((res) => res.json())
-		.then((data) => (albums = data))
-		.then(() => displayAlbums())
-		.catch((err) => console.log("Error fetching albums: " + err));
+    fetch("http://localhost:8000/api/albums")
+        .then((res) => res.json())
+        .then((data) => (albums = data))
+        .then(() => displayAlbums())
+        .catch((err) => console.log("Error fetching albums: " + err));
 }
 
 function displayAlbums() {
-	const albumTable = document.getElementById("table-body");
-	const albumItems = albums
-		.map((album, index) => albumEntry(index, album))
-		.join("");
+    const albumTable = document.getElementById("table-body");
+    const albumItems = albums
+        .map((album, index) => albumEntry(index, album))
+        .join("");
 
-	albumItems === ""
-		? (albumTable.innerHTML = `
+    albumItems === ""
+        ? (albumTable.innerHTML = `
             <tr class="text-muted mt-5">No albums found... </tr>
         `)
-		: (albumTable.innerHTML = albumItems);
+        : (albumTable.innerHTML = albumItems);
 
-	const deleteButtons = document.querySelectorAll("#delete-button");
+    const deleteButtons = document.querySelectorAll("#delete-button");
 
-	deleteButtons.forEach((deleteBtn) => {
-		deleteBtn.addEventListener("click", (e) => {
-			deleteAlbum(e.target.name);
-		});
-	});
+    deleteButtons.forEach((deleteBtn) => {
+        deleteBtn.addEventListener("click", (e) => {
+            deleteAlbum(e.target.name);
+        });
+    });
 }
 
 const albumEntry = (_, album) => {
-	return `
+    return `
     <tr>
 		<th scope="row">${album.id}</th>
 		<td>${album.name}</td>
@@ -43,15 +43,15 @@ const albumEntry = (_, album) => {
 					data-bs-toggle="modal"
 					data-bs-target="#updateAlbum"
 					onClick="toggleUpdate('${album.id}', '${album.name}', '${album.artist}', '${
-		album.year
-	}')"
+        album.year
+    }')"
 					id="update-button"
 				>
 					<i class="fa fa-pen"></i>
 				</button>
 			<button type="button" class="btn btn-danger" name="${
-				album.id
-			}" id="delete-button">
+                album.id
+            }" id="delete-button">
 				<i class="fa fa-xmark"></i>
 			</button>
 		</td>
@@ -60,118 +60,118 @@ const albumEntry = (_, album) => {
 };
 
 function deleteAlbum(id) {
-	// Show spinner and disable interactions while it resolves
-	document.getElementById("spinner").classList.remove("d-none");
-	document.getElementById("spinner").classList.add("d-flex");
-	document.body.style.pointerEvents = "none";
+    // Show spinner and disable interactions while it resolves
+    document.getElementById("spinner").classList.remove("d-none");
+    document.getElementById("spinner").classList.add("d-flex");
+    document.body.style.pointerEvents = "none";
 
-	if (!id) {
-		alert("Album id not found, try again later.");
-		document.getElementById("spinner").classList.add("d-none");
-		document.body.style.pointerEvents = "auto";
-		return;
-	}
-	fetch(`http://localhost:8000/api/albums/${id}`, {
-		method: "DELETE",
-		headers: {
-			"Content-Type": "application/json",
-		},
-	})
-		.then((res) => res.json())
-		.then((data) => {
-			if (data.message === "Collection not found.") {
-				alert("Album not found.");
-			} else {
-				alert("Album deleted.");
-				start();
-			}
-		})
-		.catch((err) => console.log("Error deleting album: " + err))
-		.finally(() => {
-			document.getElementById("spinner").classList.add("d-none");
-			document.body.style.pointerEvents = "auto";
-		});
+    if (!id) {
+        alert("Album id not found, try again later.");
+        document.getElementById("spinner").classList.add("d-none");
+        document.body.style.pointerEvents = "auto";
+        return;
+    }
+    fetch(`http://localhost:8000/api/albums/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.message === "Collection not found.") {
+                alert("Album not found.");
+            } else {
+                alert("Album deleted.");
+                start();
+            }
+        })
+        .catch((err) => console.log("Error deleting album: " + err))
+        .finally(() => {
+            document.getElementById("spinner").classList.add("d-none");
+            document.body.style.pointerEvents = "auto";
+        });
 }
 
 function addAlbum() {
-	const album = {
-		name: document.getElementById("album-name").value,
-		artist: document.getElementById("artist-name").value,
-		date: document.getElementById("year").value,
-	};
+    const album = {
+        name: document.getElementById("album-name").value,
+        artist: document.getElementById("artist-name").value,
+        date: document.getElementById("year").value,
+    };
 
-	document.getElementById("spinner").classList.remove("d-none");
-	document.getElementById("spinner").classList.add("d-flex");
-	document.body.style.pointerEvents = "none";
+    document.getElementById("spinner").classList.remove("d-none");
+    document.getElementById("spinner").classList.add("d-flex");
+    document.body.style.pointerEvents = "none";
 
-	fetch("http://localhost:8000/api/albums", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(album),
-	})
-		.then((res) => res.json())
-		.then((data) => {
-			if (data.message === "Collection already exists.") {
-				alert("Album already exists.");
-			} else {
-				alert("Album added.");
-				start();
-			}
-		})
-		.catch((err) => console.log("Error adding album: " + err))
-		.finally(() => {
-			document.getElementById("spinner").classList.add("d-none");
-			document.body.style.pointerEvents = "auto";
-		});
+    fetch("http://localhost:8000/api/albums", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(album),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.message === "Collection already exists.") {
+                alert("Album already exists.");
+            } else {
+                alert("Album added.");
+                start();
+            }
+        })
+        .catch((err) => console.log("Error adding album: " + err))
+        .finally(() => {
+            document.getElementById("spinner").classList.add("d-none");
+            document.body.style.pointerEvents = "auto";
+        });
 }
 
 function updateAlbum() {
-	const id = localStorage.getItem("albumId");
+    const id = localStorage.getItem("albumId");
 
-	const album = {
-		name: document.getElementById("album-name").value,
-		artist: document.getElementById("artist-name").value,
-		date: document.getElementById("year").value,
-	};
+    const album = {
+        name: document.getElementById("album-name").value,
+        artist: document.getElementById("artist-name").value,
+        date: document.getElementById("year").value,
+    };
 
-	document.getElementById("spinner").classList.remove("d-none");
-	document.getElementById("spinner").classList.add("d-flex");
-	document.body.style.pointerEvents = "none";
+    document.getElementById("spinner").classList.remove("d-none");
+    document.getElementById("spinner").classList.add("d-flex");
+    document.body.style.pointerEvents = "none";
 
-	fetch(`http://localhost:8000/api/albums/${id}`, {
-		method: "PUT",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(album),
-	})
-		.then((res) => res.json())
-		.then((data) => {
-			if (data.message === "Collection not found.") {
-				alert("Album not found.");
-			} else {
-				alert("Album updated.");
-				start();
-			}
-		})
-		.catch((err) => console.log("Error updating album: " + err))
-		.finally(() => {
-			document.getElementById("spinner").classList.add("d-none");
-			document.body.style.pointerEvents = "auto";
-		});
+    fetch(`http://localhost:8000/api/albums/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(album),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.message === "Collection not found.") {
+                alert("Album not found.");
+            } else {
+                alert("Album updated.");
+                start();
+            }
+        })
+        .catch((err) => alert("Error updating album: " + err))
+        .finally(() => {
+            document.getElementById("spinner").classList.add("d-none");
+            document.body.style.pointerEvents = "auto";
+        });
 }
 
 document.querySelectorAll("#update-album").forEach((updateAlbumBtn) => {
-	updateAlbumBtn.addEventListener("click", () => {
-		updateAlbum();
-	});
+    updateAlbumBtn.addEventListener("click", () => {
+        updateAlbum();
+    });
 });
 
 document.querySelectorAll("#add-album").forEach((addAlbumBtn) => {
-	addAlbumBtn.addEventListener("click", () => {
-		addAlbum();
-	});
+    addAlbumBtn.addEventListener("click", () => {
+        addAlbum();
+    });
 });
 start();
